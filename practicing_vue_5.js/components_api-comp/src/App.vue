3bda741f7f2924e13420a665968e8359c0530f07
Renata <renatamoss.web@gmail.com>
2022-04-06 11:34:32 -0300

    novos conceitos curso vue3 avançado

---
+++ practicing_vue_5.js/components_api-comp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <HelloWorld 
+  :msg="msg"
+  @minha-messagem="alertaMessagem" />
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+
+//props: ação executada no filho, dados do pai
+const msg = ref("Estou estudando components com API Composition");
+
+
+//emit: ação executada no pai, ordem do filho
+const alertaMessagem = (msgEmit) => {
+  alert(msgEmit)
+}
+
+
+</script>
+
+
---
+++ practicing_vue_5.js/components_api-comp/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1 @click="chamaMsg">{{ msg }}</h1>
+    <button @click="enviaMsg">CLICA AQUI</button>
+  </div>
+</template>
+
+<script setup>
+import { watch, ref } from "vue";
+
+
+//pai para filho(via props)
+const props = defineProps({
+  msg: String,
+});
+
+const chamaMsg = () => {
+  alert(props.msg);
+};
+
+
+
+//filho para pai(via evento)
+const emit = defineEmits(["minha-messagem"]);
+
+const enviaMsg = () => {
+  emit("minha-messagem", "Estudando emit");
+};
+
+
+</script>
+
+
+
+
+
